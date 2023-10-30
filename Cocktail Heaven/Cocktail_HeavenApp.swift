7b214454ce41064cde2aa1dbe301f0f20612059c
Kirill Sivokhin <kirill.sivokhin@vk.com>
2023-10-30 22:18:40 +0300

    initial commit

---
+++ Cocktail Heaven/Cocktail_HeavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cocktail_HeavenApp.swift
+//  Cocktail Heaven
+//
+//  Created by Kirill Sivokhin on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Cocktail_HeavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
